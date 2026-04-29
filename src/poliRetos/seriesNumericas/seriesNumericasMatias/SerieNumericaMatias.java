b4ecf3714c9bacb31754d3aabd67d5bcdee67a5f
Baxter18 <matiasenriquez2007gmail.com>
2026-04-28 19:24:06 -0500

    Desarrollo de Serie Numerica

---
+++ src/poliRetos/seriesNumericas/seriesNumericasMatias/SerieNumericaMatias.java
@@ -0,0 +1,18 @@
+package poliRetos.seriesNumericas.seriesNumericasMatias;
+import java.util.Scanner;
+
+public class SerieNumericaMatias {
+   
+    public void generarSerie() {;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el numero de terminos a generar: ");
+        int num = sc.nextInt();
+        
+        System.out.println("Serie numerica: ");
+        
+        for (int i = 1; i <= num; i++) {
+            System.out.print(i * i + " ");
+        }
+    System.out.println();
+    }
+} 
