bae7c30fe9dc434d491bdffffad5af376387c4c3
jhonatan050 <jhonatanchango9@gmail.com>
2026-05-01 18:18:19 -0500

    feat: Agregar series numericas y de caracteres.

--- src/poliRetos/SerieCaracteres/serieCaracteresJhonatan.java
+++ src/poliRetos/SerieCaracteres/serieCaracteresJhonatan.java
@@ -1,5 +1,22 @@
 package poliRetos.SerieCaracteres;
 
 public class SerieCaracteresJhonatan {
-
-}
+    /**
+     * Generar la serie de caracteres S4: + - * / + - * / ......
+     * @param cantidad: Se encarga de determinar la cantidad de elementos a generar en la serie.
+     */
+    public void generarSerieCaracteres(int cantidad){
+        for(int i=0;i<cantidad; i++){
+                if(i%4==0){
+                    System.out.print("+");
+                }else if(i%4==1){
+                    System.out.print("-");
+                }else if(i%4==2){
+                    System.out.print("*");
+                }else if(i%4==3){
+                    System.out.print("/");
+                }   
+            }
+        }
+      
+    }
--- src/poliRetos/seriesNumericas/SerieNumericaJhonatan.java
+++ src/poliRetos/seriesNumericas/SerieNumericaJhonatan.java
@@ -1,5 +1,24 @@
 package poliRetos.seriesNumericas;
 
 public class SerieNumericaJhonatan {
+    /**
+     * Generar la serie numerica S4: Fibonacci / Pares
+     * @param cantidadElementos
+     */
+    public void generarSerie(int cantidadElementos){
+        
+        int a=0;
+        int b=1;
+        int denominador=2;
 
+        for(int i=0; i<cantidadElementos; i++){
+            System.out.print(a+"/"+denominador+",");
+
+            int sigTermino = a+b;
+            a = b;
+            b = sigTermino;
+
+            denominador+=2;
+        }
+    }
 }
