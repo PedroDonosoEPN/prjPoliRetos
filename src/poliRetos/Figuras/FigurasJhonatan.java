c93cae0ec32cbb91105198576c0893e757402b26
jhonatan050 <jhonatanchango9@gmail.com>
2026-05-01 23:23:10 -0500

    feat: agregar figura F4

--- src/poliRetos/Figuras/FigurasJhonatan.java
+++ src/poliRetos/Figuras/FigurasJhonatan.java
@@ -1,5 +1,20 @@
 package poliRetos.Figuras;
 
 public class FigurasJhonatan {
-
+    /**
+     * Generar la figura F4: Triangulo rectangulo con asteriscos
+     * @param tamano: Define el tamaño que va a tener la figura generada.
+     */
+    public void generarFigura(int tamano){
+        for(int v=0; v<tamano; v++){
+            for(int h=0; h<tamano; h++){
+                if(h+v>= tamano-1){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
