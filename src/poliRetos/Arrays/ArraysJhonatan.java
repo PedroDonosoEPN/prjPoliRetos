5087b3867e8392a436ccfe1ebb0e27c5ad10b8a4
jhonatan050 <jhonatanchango9@gmail.com>
2026-05-03 15:19:03 -0500

    feat: Agregar loading L04 y arrays A04

--- src/poliRetos/Arrays/ArraysJhonatan.java
+++ src/poliRetos/Arrays/ArraysJhonatan.java
@@ -1,5 +1,36 @@
 package poliRetos.Arrays;
 
 public class ArraysJhonatan {
+    /**
+     * A04: Crear una matriz donde se forme una x con su nombre y apellido
+     * @param nombre
+     * @param apellido
+     */
+    public void generarMatrizX(String nombre, String apellido){
 
+        int n = Math.max(nombre.length(), apellido.length());
+        char[][] matriz = new char[n][n];
+
+        for (int i =0; i<n;i++){
+            for (int j=0; j<n;j++){
+                matriz[i][j] = ' ';
+            }
+        }
+
+        for (int i =0; i<n; i++){
+            if (i < nombre.length()){
+                matriz[i][i] = nombre.charAt(i);
+            }
+            if (i< apellido.length()){
+                matriz[i][n-1-i] = apellido.charAt(i);
+            }
+        }
+
+        for(int i =0; i<n; i++){
+            for (int j=0; j<n; j++){
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/poliRetos/Loading/LoadingJhonatan.java
+++ src/poliRetos/Loading/LoadingJhonatan.java
@@ -1,5 +1,19 @@
 package poliRetos.Loading;
 
 public class LoadingJhonatan {
-
+    /**
+     * L04: Waiting que avanza de 0% a 100% haciendo uso de la figura oOo para simular movimiento
+     * @param avance determina hasta que porcentaje va a correr el waiting. 
+     */
+    public void mostrarWaiting(int avance){
+        String waiting[]={"Ooo","oOo","ooO","oOo","Ooo"};
+        for(int i = 0; i<avance;i++){
+            System.out.print("\r"+waiting[i %waiting.length]);
+            System.out.print(" "+(i+1)+" %");
+            try {
+                Thread.sleep(100);
+            } catch (Exception e){} 
+        }
+        System.out.println();
+    }
 }
