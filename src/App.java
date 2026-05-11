ef7f636c310bd2cd915dfbcc2119fb6f06406408
PedroDonosoEPN <pedro.donoso@epn.edu.ec>
2026-05-10 23:58:22 -0500

    main:Modificacion App

--- src/App.java
+++ src/App.java
@@ -1,6 +1,18 @@
 
 public class App {
         public static void main(String[] args) {
+
+        System.out.println("---------------------------------------");
+        System.out.println("            GRUPO NRO. 2               ");
+        System.out.println("---------------------------------------");
+        System.out.println(" INTEGRANTES:");
+        System.out.println(" - Jhonatan Chango");
+        System.out.println(" - Matias Enriquez");
+        System.out.println(" - Pedro Donoso");
+        System.out.println(" - Mateo Cardenas");
+        System.out.println(" - Eidan Clavijo");
+        System.out.println("---------------------------------------");
+        System.out.println(); // Espacio estético
         Controlador controlador = new Controlador();
         controlador.showDeberGrupal();
 
--- src/Controlador.java
+++ src/Controlador.java
@@ -2,14 +2,10 @@ public  class   Controlador {
     
     public void showDeberGrupal(){
         ControladorPedro controladorPedro = new ControladorPedro();
-        //Serie Numerica
         controladorPedro.showDeber();
         
         ControladorJhonatan controladorJhonatan = new ControladorJhonatan();
         controladorJhonatan.mostrarDeberJhonatan();
-
-        ControladorMatias controladorMatias = new ControladorMatias();
-        controladorMatias.mostrarDeberMatias();
 
     }
 }
