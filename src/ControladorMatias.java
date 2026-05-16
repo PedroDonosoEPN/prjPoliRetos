b8b0f3c6d7882209077c698f35e7871c21a93f78
Baxter18 <matiasenriquez2007gmail.com>
2026-05-16 18:14:03 -0500

    Desarrollo de For, Do While y While en Series Numericas, Alfabeticas y Recursion

--- src/ControladorMatias.java
+++ src/ControladorMatias.java
@@ -10,12 +10,16 @@ public class ControladorMatias {
     public void mostrarDeberMatias() {
 
         // 1. Series Numéricas 
-        SerieNumericaMatias sNum = new SerieNumericaMatias();
-        sNum.generarSerie(10); 
+        SerieNumericaMatias SerieNumerica = new SerieNumericaMatias();
+        SerieNumerica.generarSerieFor(10); 
+        SerieNumerica.generarSerieWhile(10);
+        SerieNumerica.generarSerieDoWhile(10);
 
         // 2. Series de Caracteres
-        SerieAlfabeticaMatias sAlfa = new SerieAlfabeticaMatias();
-        sAlfa.generarAlfabeto(12);
+        SerieAlfabeticaMatias SerieAlfabetica = new SerieAlfabeticaMatias();
+        SerieAlfabetica.generarAlfabetoFor(12);
+        SerieAlfabetica.generarAlfabetoWhile(12);
+        SerieAlfabetica.generarAlfabetoDoWhile(12);
 
         // 3. Figuras (Pirámide)
         PiramideCaracterMatias figuras = new PiramideCaracterMatias();
@@ -27,7 +31,9 @@ public class ControladorMatias {
 
         // 5. Recursión
         RecursionMatias recursion = new RecursionMatias();
-        recursion.conteoRegresivo(5);
+        recursion.conteoRegresivoFor(5);
+        recursion.conteoRegresivoWhile(5);
+        recursion.conteoRegresivoDoWhile(5);
 
         // 6. Grafos y Autómatas
         GrafosYAutomatasMatias automatnas = new GrafosYAutomatasMatias();
