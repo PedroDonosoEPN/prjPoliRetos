63d2f92ed16bfe648dccda561768acfa2bbdcb8e
Baxter18 <164462237+Baxter18@users.noreply.github.com>
2026-05-19 08:26:26 -0500

    Correcion en RecursionMatias

--- src/Recursion/RecursionMatias.java
+++ src/Recursion/RecursionMatias.java
@@ -8,28 +8,35 @@ public class RecursionMatias {
          * @param numero El número desde el cual iniciar el conteo regresivo.
         */
 
-        for (int i = numero; i >= 0; i--) {
-            System.out.print(i + " ");
+        if (numero < 0) {
+            return; 
         }
-        System.out.println();
-    }   
+        System.out.print(numero + " ");
+
+        for (int i = 0; i < 1; i++) {
+            conteoRegresivoFor(numero - 1);
+        }
+        System.out.println("");
+    }  
 
     public void conteoRegresivoWhile(int numero) {
-        int i = numero;
-        while (i >= 0) {
-            System.out.print(i + " ");
-            i--;
+        while (numero >= 0) {
+            System.out.print(numero + " ");
+            conteoRegresivoWhile(numero - 1);
+            return;
         }
-        System.out.println();
+        System.out.println("");
     }
 
     public void conteoRegresivoDoWhile(int numero) {
-        int i = numero;
-        do {
-            System.out.print(i + " ");
-            i--;
-        } while (i >= 0);
-        System.out.println();
-    }
-}
+        if (numero < 0) {
+            return;
+        }
 
+        do {
+            System.out.print(numero + " ");
+            conteoRegresivoDoWhile(numero - 1);
+        } while (false);
+        System.out.println("");
+    }      
+}
